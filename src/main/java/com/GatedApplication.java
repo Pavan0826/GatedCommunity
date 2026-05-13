ba832e83e8426dd6daa1a996fc3abf8dbb276f68
pavankalyan <pavankalyan0826@gmail.com>
2026-05-13 18:15:11 +0530

    Getting started with project and initial setup was done

---
+++ src/main/java/com/GatedApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatedApplication.class, args);
+	}
+
+}
