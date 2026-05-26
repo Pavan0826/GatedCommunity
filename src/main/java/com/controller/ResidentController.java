45233d565928f3a49f22f9df401ea6cd10fe4f37
pavankalyan <pavankalyan0826@gmail.com>
2026-05-26 23:07:43 +0530

    no message

--- src/main/java/com/controller/ResidentController.java
+++ src/main/java/com/controller/ResidentController.java
@@ -35,7 +35,7 @@ public class ResidentController {
 			Resident resident = auth.get();
 			session.setAttribute("resident", resident);
 			model.addAttribute("message", "Login successful");
-			// After login, redirect resident to the complaint list page (shows complaints and Add button)
+			
 			return "redirect:/resident/complaint/list";
 		}
 
